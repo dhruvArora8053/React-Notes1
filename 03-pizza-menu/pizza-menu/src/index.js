9c6c8f73c5244a0ae36f3cdbaf5ea16ab12d4b44
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-20 20:48:13 +0530

    ternary operator implemented

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -81,13 +81,13 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas>0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
-      )}
+      ) : <p>We're still working on our menu. Please come back later :)</p>}
 
       {/* <Pizza
         name="Pizza Spinaci"
