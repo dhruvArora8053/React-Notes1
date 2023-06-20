1c5e650c664af4c16b557ebe6f5ae2b21dd1285b
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-20 20:36:52 +0530

    conditional rendering

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -74,6 +74,8 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
@@ -127,7 +129,13 @@ function Footer() {
   // return React.createElement('footer', null, "We're currently open!")
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}We're currently open
+      {/* {new Date().toLocaleTimeString()}We're currently open */}
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 }
