9af956c280b983e5e2788165db97ff9dad0bd357
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-20 21:22:40 +0530

    destructuring props

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -108,18 +108,19 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  console.log(props);
+function Pizza({ pizzaObj }) {
+  // console.log(props);
+  console.log(pizzaObj);
 
-  if (props.pizzaObj.soldOut) return null;
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
       </div>
     </li>
   );
@@ -148,7 +149,7 @@ function Footer() {
     <footer className="footer">
       {/* {new Date().toLocaleTimeString()}We're currently open */}
       {isOpen ? (
-        <Order closeHours={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
@@ -158,12 +159,11 @@ function Footer() {
   );
 }
 
-function Order(props) {
+function Order({ closeHour, openHour }) {
+  // console.log(props);
   return (
     <div className="order">
-      <p> 
-        We're open until {props.closeHours}:00. Come visit us or order online
-      </p>
+      <p>We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online</p>
       <button className="btn">Order</button>
     </div>
   );
