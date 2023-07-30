9ee49f296473ab42d287afe9cd8a8fb6fa0b4161
Dhruv Arora <dhruvarora.code@gmail.com>
2023-07-30 20:32:10 +0530

    form add friend removed

--- 06-eat-n-split/eat-n-split/src/App.js
+++ 06-eat-n-split/eat-n-split/src/App.js
@@ -87,17 +87,3 @@ function Friend({ friend }) {
   );
 }
 
-function FormAddFriend() {
-  return (
-    <form className="form-add-friend">
-      <label>🧑‍🤝‍🧑Friend name</label>
-      <input type="text " />
-
-      <label>🪟Image URL</label>
-      <input type="text" />
-
-      <Button>Add</Button>
-    </form>
-  );
-}
-
