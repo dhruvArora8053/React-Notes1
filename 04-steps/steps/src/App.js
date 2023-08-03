85672584b6b14a675e230fe3702ea2f3a5f2dc33
Dhruv Arora <dhruvarora.code@gmail.com>
2023-08-03 18:52:54 +0530

    implementing children props

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -80,18 +80,13 @@ export default function App() {
 
           <div className="buttons">
             {/* adding listeners */}
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handlePrevious}
-              text="Previous"
-            />
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
           </div>
         </div>
       )}
@@ -99,13 +94,13 @@ export default function App() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, text }) {
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onclick}
+      onClick={onClick}
     >
-      {text}
+      {children}
     </button>
   );
 }
