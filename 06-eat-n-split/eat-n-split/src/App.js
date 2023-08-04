4306082968ba624eb659e6865fc473713eaa8a7a
Dhruv Arora <dhruvarora.code@gmail.com>
2023-08-04 15:55:59 +0530

    implemented static part

--- 06-eat-n-split/eat-n-split/src/App.js
+++ 06-eat-n-split/eat-n-split/src/App.js
@@ -27,7 +27,10 @@ export default function App() {
       <div className="sidebar">
         <FriendsList />
         <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -87,3 +90,27 @@ function FormAddFriend() {
   );
 }
 
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>🕴️ Your Expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 X's Expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
