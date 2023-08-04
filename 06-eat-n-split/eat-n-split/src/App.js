6072c673640e8846027edf9d53dcab9bed727b04
Dhruv Arora <dhruvarora.code@gmail.com>
2023-08-04 15:21:21 +0530

    implemented list of friends

--- 06-eat-n-split/eat-n-split/src/App.js
+++ 06-eat-n-split/eat-n-split/src/App.js
@@ -37,12 +37,33 @@ function FriendsList() {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} />
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
   );
 }
 
 function Friend({ friend }) {
-  return <li>{friend.name}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
