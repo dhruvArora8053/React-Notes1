ae70b75b25856dd0d7b71e76a2a36b9abbf75b95
Dhruv Arora <dhruvarora.code@gmail.com>
2023-08-06 23:43:05 +0530

    implemented star rating component

--- 07-usepopcorn/usepopcorn/src/StarRating.js
+++ 07-usepopcorn/usepopcorn/src/StarRating.js
@@ -1,18 +1,28 @@
-export default function StarRating() {
-  const containerStyle = {
-    display: "flex",
-    alignItems: "center",
-    gap: "16px",
-  };
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
 
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+export default function StarRating({ maxRating=5 }) {
   return (
     <div style={containerStyle}>
-      <div>
-        {Array.from({ length: 5 }, (_, i) => (
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
           <span>S{i + 1}</span>
         ))}
       </div>
-      <p>10</p>
+      <p style={textStyle}>10</p>
     </div>
   );
 }
