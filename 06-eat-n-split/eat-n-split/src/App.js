9c9fe945d2c783dce25d39c5d5798f45e76e1598
Dhruv Arora <dhruvarora.code@gmail.com>
2023-08-10 20:25:48 +0530

    split bill implemented

--- 06-eat-n-split/eat-n-split/src/App.js
+++ 06-eat-n-split/eat-n-split/src/App.js
@@ -29,6 +29,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -82,6 +83,31 @@ function FormAddFriend() {
 
       <label>🖼️ Image URL</label>
       <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🕴️ Your expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
 
       <Button>Add</Button>
     </form>
