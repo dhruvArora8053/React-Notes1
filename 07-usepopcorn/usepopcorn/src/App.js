18d97587c156d883561246e93747ef471c0fb487
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-04 10:38:22 +0530

    passed elements as props

--- 07-usepopcorn/usepopcorn/src/App.js
+++ 07-usepopcorn/usepopcorn/src/App.js
@@ -64,7 +64,7 @@ export default function App() {
         <Box>
           <MovieList movies={movies} />
         </Box>
-        
+
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
